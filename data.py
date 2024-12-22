3deaaa691ada9136c52175fc3d91df417bb01810
Faiq Khan <157122105+FakerPK@users.noreply.github.com>
2024-12-22 12:28:38 +0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,62 @@
+config = [
+    {
+        "usertoken": "AUTH_TOKEN_1",
+        "nodes": [
+            {
+                "nodeId": "PUB_KEY_1",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_2",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_3",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_4",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_4",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+        ]
+    },
+    {
+        "usertoken": "AUTH_TOKEN_2",
+        "nodes": [
+            {
+                "nodeId": "PUB_KEY_1",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_2",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_3",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_4",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            },
+            {
+                "nodeId": "PUB_KEY_5",
+                "hardwareId": "HARDWARE_ID", #stays same for all nodes
+                "proxy": ""  #format: socks5://username:pass@ip:port
+            }
+        ]
+    } #add a comma and copy paste if you want to add more auth tokens
+]
